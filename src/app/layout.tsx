258e2697234f9253af63e689f7fff72ed2bc110b
tonipcv <xppsalvador@gmail.com>
2025-01-20 18:29:44 -0300

    17-2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Quicksand } from "next/font/google";
+import { Inter, Quicksand } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 const quicksand = Quicksand({ 
@@ -37,7 +32,7 @@ export default function RootLayout({
         `}</style>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-quicksand`}
+        className={`${inter.variable} antialiased font-quicksand`}
       >
         {children}
       </body>
